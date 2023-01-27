ec1aee882e48835b00e1e715fc4e4916cad6ab04
Ben Vale <info@benvale.me>
2023-01-27 09:05:07 +0000

    splitting out standard bar from screens

--- qtile/config.py
+++ qtile/config.py
@@ -5,12 +5,11 @@ import os
 import subprocess
 
 
-#################
-### Initial variables ###
-#################
+################
+### Modifier ###
+################
 
 mod = "mod4"
-terminal = "alacritty"
 
 
 ######################
@@ -43,6 +42,7 @@ elif qtile.core.name == "wayland":
 Takes the start variable above and runs it.
 """
 
+
 @hook.subscribe.startup_once
 def autostart():
     home = os.path.expanduser(start)
@@ -70,47 +70,72 @@ elif wayland:
 keys = [
 
     # Switch things
-    Key([mod], "Left", lazy.layout.left(),desc="Move focus to left"),
-    Key([mod], "Right", lazy.layout.right(),desc="Move focus to right"),
-    Key([mod], "Down", lazy.layout.down(),desc="Move focus down"),
-    Key([mod], "Up", lazy.layout.up(),desc="Move focus up"),
-    Key([mod], "space", lazy.layout.next(),desc="Move window focus to other window"),
-    Key([mod], "period", lazy.next_screen(), desc="Move focus to other screen"),
+    Key([mod], "Left", lazy.layout.left(),
+        desc="Move focus to left"),
+    Key([mod], "Right", lazy.layout.right(),
+        desc="Move focus to right"),
+    Key([mod], "Down", lazy.layout.down(),
+        desc="Move focus down"),
+    Key([mod], "Up", lazy.layout.up(),
+        desc="Move focus up"),
+    Key([mod], "space", lazy.layout.next(),
+        desc="Move window focus to other window"),
+    Key([mod], "period", lazy.next_screen(),
+        desc="Move focus to other screen"),
 
     # Move things
-    Key([mod, "shift"], "Left", lazy.layout.shuffle_left(),desc="Move window to the left"),
-    Key([mod, "shift"], "Right", lazy.layout.shuffle_right(),desc="Move window to the right"),
-    Key([mod, "shift"], "Down", lazy.layout.shuffle_down(),desc="Move window down"),
-    Key([mod, "shift"], "Up", lazy.layout.shuffle_up(),desc="Move window up"),
+    Key([mod, "shift"], "Left", lazy.layout.shuffle_left(),
+        desc="Move window to the left"),
+    Key([mod, "shift"], "Right", lazy.layout.shuffle_right(),
+        desc="Move window to the right"),
+    Key([mod, "shift"], "Down", lazy.layout.shuffle_down(),
+        desc="Move window down"),
+    Key([mod, "shift"], "Up", lazy.layout.shuffle_up(),
+        desc="Move window up"),
 
-    # Grow things
-    Key([mod, "control"], "Left", lazy.layout.grow_left(),desc="Grow window to the left"),
-    Key([mod, "control"], "Right", lazy.layout.grow_right(),desc="Grow window to the right"),
-    Key([mod, "control"], "Down", lazy.layout.grow_down(),desc="Grow window down"),
-    Key([mod, "control"], "Up", lazy.layout.grow_up(), desc="Grow window up"),
-    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+    # Grow things (doesn't work in monad)
+    Key([mod, "control"], "Left", lazy.layout.grow_left(),
+        desc="Grow window to the left"),
+    Key([mod, "control"], "Right", lazy.layout.grow_right(),
+        desc="Grow window to the right"),
+    Key([mod, "control"], "Down", lazy.layout.grow_down(),
+        desc="Grow window down"),
+    Key([mod, "control"], "Up", lazy.layout.grow_up(),
+        desc="Grow window up"),
+    Key([mod], "n", lazy.layout.normalize(),
+        desc="Reset all window sizes"),
 
     # Launch things
-    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
-    Key([mod], "d", lazy.spawn(launcher), desc="Launch application launcher"),
+    Key([mod], "Return", lazy.spawn("alacritty"),
+        desc="Launch terminal"),
+    Key([mod], "d", lazy.spawn(launcher),
+        desc="Launch application launcher"),
 
     # Toggle things
-    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
-    Key([mod], "f", lazy.window.toggle_floating(), desc="Toggle floating mode"),
+    Key([mod], "Tab", lazy.next_layout(),
+        desc="Toggle between layouts"),
+    Key([mod], "f", lazy.window.toggle_floating(),
+        desc="Toggle floating mode"),
 
     # Kill things
-    Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
-    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+    Key([mod], "w", lazy.window.kill(),
+        desc="Kill focused window"),
+    Key([mod, "control"], "q", lazy.shutdown(),
+        desc="Shutdown Qtile"),
 
     # Reload things
-    Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
+    Key([mod, "control"], "r", lazy.reload_config(),
+        desc="Reload the config"),
 
     # Spawn things
-    Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+    Key([mod], "r", lazy.spawncmd(),
+        desc="Spawn a command using a prompt widget"),
 
     # XF86 things
-    Key([], "XF86MonBrightnessUp", lazy.spawn("brightnessctl set +10"), desc="Turn the brightness up"),
-    Key([], "XF86MonBrightnessDown", lazy.spawn("brightnessctl set 10-"), desc="Turn the brightness up")
+    Key([], "XF86MonBrightnessUp", lazy.spawn("brightnessctl set +10"),
+        desc="Turn the brightness up"),
+    Key([], "XF86MonBrightnessDown", lazy.spawn("brightnessctl set 10-"),
+        desc="Turn the brightness up")
 ]
 
 
@@ -125,25 +150,21 @@ for i in groups:
         [
             # mod1 + letter of group = switch to group
             Key(
-                [mod],
-                i.name,
-                lazy.group[i.name].toscreen(),
-                desc="Switch to group {}".format(i.name),
+                [mod],i.name,lazy.group[i.name].toscreen(),
+                    desc="Switch to group {}".format(i.name),
             ),
             # mod1 + shift + letter of group = move focused window to group
-            Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-                desc="move focused window to group {}".format(i.name)),
+            Key(
+                [mod, "shift"], i.name, lazy.window.togroup(i.name),
+                    desc="move focused window to group {}".format(i.name)
+            ),
         ]
     )
 
 
 ################
-### Defaults ###
+### Layouts ###
 ################
-
-"""
-TODO: Rip the bar stuff out of the screens just do it all in one go here.
-"""
 
 layout_defaults = {
     "border_focus": "#000000",
@@ -160,38 +181,100 @@ layouts = [
     layout.Floating(**layout_defaults)
 ]
 
+
+######################################
+### Widgets and Extension Defaults ###
+######################################
+
 widget_defaults = {
-    "font": "Lexend Bold",
-    "fontshadow":"000000",
+    "font": "Hack",
+    "fontshadow": "000000",
     "fontsize": 13,
     "padding": 3
 }
+
 extension_defaults = widget_defaults.copy()
 
-separator_defaults = {
-    "linewidth": 1,
-    "size_percent": 50,
-    "padding": 20
-}
 
-groupbox_defaults = {
-    "highlight_method":"line",
-    "borderwidth":5,
-    "highlight_color":['00000000', '87CEEB'],
-    "this_current_screen_border":'87CEEB',
-    "this_screen_border":'4682B4',
-    "other_current_screen_border":'87CEEB',
-    "other_screen_border":'676767',
-    "padding_x":7,
-    "margin_x":1
-}
+##########################
+### Wallpaper Defaults ###
+##########################
 
-clock_defaults = {
-    "format":"%Y-%m-%d %a %H:%M:%S"
-}
-
-wallpaper_default = 'Wallpaper/13.jpg'
+wallpaper_default = 'Wallpaper/3.jpg'
 wallpaper_mode_defalt = 'stretch'
+
+
+###########
+### Bar ###
+###########
+
+def aBar():
+    separator_defaults = {
+        "linewidth": 1,
+        "size_percent": 50,
+        "padding": 20
+    }
+    thisBar = bar.Bar(
+        [
+            widget.GroupBox(
+                highlight_method='line',
+                highlight_color = ['00000000', '87CEEB'],
+                this_current_screen_border='87CEEB',
+                this_screen_border='4682B4',
+                other_current_screen_border='87CEEB',
+                other_screen_border='676767',
+                rounded=False
+            ),
+            widget.Spacer(
+                length=50
+            ),
+            widget.Prompt(
+                prompt="[benv@archlinux ~]$ ",
+                font="Hack",
+                cursorblink=0.3
+            ),
+            widget.Spacer(),
+            widget.Sep(
+                **separator_defaults
+            ),
+            widget.CapsNumLockIndicator(),
+            widget.Sep(
+                **separator_defaults
+            ),
+            widget.PulseVolume(
+                fmt="VOL:{}"
+            ),
+            widget.Sep(
+                **separator_defaults
+            ),
+            widget.Wlan(
+                format="NET: {essid} ({percent:2.0%})"
+            ),
+            widget.Net(
+                format="{down} ↓↑ {up}", prefix="M"
+            ),
+            widget.Sep(
+                **separator_defaults
+            ),
+            widget.Battery(
+                format="BATTERY: {percent:2.0%} ({hour:d}:{min:02d})"),
+            widget.Sep(
+                **separator_defaults
+            ),
+            widget.Clock(
+                format="%Y-%m-%d %H:%M:%S"
+            ),
+            widget.Sep(
+                **separator_defaults
+            ),
+            widget.CurrentLayoutIcon(
+                scale=0.5
+            )
+        ],
+        30,
+        background=["00000000"]
+    )
+    return thisBar
 
 
 ###############
@@ -206,103 +289,19 @@ screens = [
     Screen(
         wallpaper=wallpaper_default,
         wallpaper_mode=wallpaper_mode_defalt,
-        bottom=bar.Bar(
-            [
-                widget.GroupBox(
-                    **groupbox_defaults
-                ),
-                widget.Spacer(
-                    length=50
-                ),
-                widget.Prompt(
-                    prompt="[benv@archlinux ~]$ ",
-                    font="Hack Bold",
-                    cursorblink=0.3
-                ),
-                widget.Spacer(),
-                widget.PulseVolume(
-                    fmt="VOL:{}"
-                ),
-                widget.Sep(
-                    **separator_defaults
-                ),
-                widget.Wlan(
-                    format="NET: {essid} ({percent:2.0%})"
-                ),
-                widget.Net(
-                    format="{down} ↓↑ {up}",
-                    prefix="M"
-                ),
-                widget.Sep(
-                    **separator_defaults
-                ),
-                widget.Battery(
-                    format="BATTERY: {percent:2.0%} ({hour:d}:{min:02d})"
-                ),
-                widget.Sep(
-                    **separator_defaults
-                ),
-                widget.Clock(
-                    **clock_defaults
-                ),
-                widget.Sep(
-                    **separator_defaults
-                ),
-                widget.CurrentLayoutIcon(
-                    scale=0.5
-                )
-            ],
-            30,
-            background=["00000000","000000"]
-        )
+        bottom=aBar()
     ),
     Screen(
         wallpaper=wallpaper_default,
         wallpaper_mode=wallpaper_mode_defalt,
-        bottom=bar.Bar(
-            [
-                widget.GroupBox(
-                    **groupbox_defaults
-                ),
-                widget.Spacer(),
-                widget.Clock(
-                    **clock_defaults
-                ),
-                widget.Sep(
-                    **separator_defaults
-                ),
-                widget.CurrentLayoutIcon(
-                    scale=0.5
-                )
-            ],
-            30,
-            background=["00000000","000000"]
-            )
+        bottom=aBar()
     ),
     Screen(
         wallpaper=wallpaper_default,
         wallpaper_mode=wallpaper_mode_defalt,
-        bottom=bar.Bar(
-            [
-                widget.GroupBox(
-                    **groupbox_defaults
-                ),
-                widget.Spacer(),
-                widget.Clock(
-                    **clock_defaults
-                ),
-                widget.Sep(
-                    **separator_defaults
-                ),
-                widget.CurrentLayoutIcon(
-                    scale=0.5
-                )
-            ],
-            30,
-            background=["00000000","000000"])
+        bottom=aBar()
     )
 ]
-
 
 
 """
